bedf71801dfb1c603efb3ceb1d7adbd13ebf1055
talahan99 <4taco@hieu.in>
2024-05-27 07:06:36 -0400

    new commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'trevor alahan';
+export const SITE_DESCRIPTION = 'portfolio of a student software engineer';
